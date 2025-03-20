85d0f3eb746552b75b43bb6bdf0033576db8b5f3
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-19 20:47:10 -0400

    updated home pasge

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -6,9 +6,9 @@ const Home: React.FC = () => {
     navigate("/search");
   };
   return (
-    <div>
-      <div className="text-black bg-blue-400 w-full h-20">
-        <h1>title</h1>
+    <div className="bg-pink-300 px-40 flex h-[calc(100vh-100px)] w-full">
+      <div className="text-black w-full flex space-y-5 flex-col mt-40 items-start justify-start">
+        <h1 className="text-3xl font-bold">title</h1>
         <p>paragraph</p>
         <button onClick={goSearch}>search</button>
       </div>
