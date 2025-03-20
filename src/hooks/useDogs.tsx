52fbe120b5b377529ace2c8fc8114352ae54ce39
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-19 21:26:02 -0400

    impoprted dog type

--- src/hooks/useDogs.tsx
+++ src/hooks/useDogs.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { SearchResults } from "../types";
+import { Dog, SearchResults } from "../types";
 
 export const useDogs = () => {
   //we need errors, loading, breeds, dogs, and search results
   const [error, setError] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [breeds, setBreeds] = useState<string[]>([]);
-  const [dogs, setDogs] = useState<string[]>([]);
+  const [dogs, setDogs] = useState<Dog[]>([]);
   const [searchResults, setSearchResults] = useState<SearchResults | null>(
     null,
   );
