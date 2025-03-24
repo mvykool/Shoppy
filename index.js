b3f19d1cc36a4ae15cce584c106b06fdafe56aa5
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-24 03:53:43 -0400

    updated navlinks functionality

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // select queries
 //select main content div where all the content will be replaced
 const mainContent = document.querySelector("#main-section");
-const shopLink = document.querySelector(".shopLink");
+const navLinks = document.querySelectorAll(".nav-link");
 const homeLink = document.querySelector(".homeLink");
 //initial home page
 const homePage = homeLink.getAttribute("data-page");
 
 //create a function that retrives the proper file based on the data-page
 
+console.log(navLinks);
 async function pageLoader(page) {
   //now i have to load the pages that matches the data-page
   const response = await fetch(page);
@@ -19,12 +20,15 @@ async function pageLoader(page) {
   history.pushState({ page: page }, null, `#${page.slice(11)}`);
 }
 
-shopLink.addEventListener("click", (e) => {
-  e.preventDefault();
-  //get the data-page since the html file will be set there
-  const page = shopLink.getAttribute("data-page");
-  //pass page to the pageloader function
-  pageLoader(page);
+navLinks.forEach((page) => {
+  page.addEventListener("click", (e) => {
+    e.preventDefault();
+    //get the data-page since the html file will be set there
+
+    const pageView = page.getAttribute("data-page");
+    //pass page to the pageloader function
+    pageLoader(pageView);
+  });
 });
 
 //create the initial state for the URL
