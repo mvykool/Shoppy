a81de83b9e4257d6e03dc75ac5dafa48b8a5a091
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-23 23:55:02 -0400

    updated js and added home view

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 //select main content div where all the content will be replaced
 const mainContent = document.querySelector("#main-section");
 const shopLink = document.querySelector(".shopLink");
+const homeLink = document.querySelector(".homeLink");
+//initial home page
+const homePage = homeLink.getAttribute("data-page");
 
 //create a function that retrives the proper file based on the data-page
 
@@ -12,11 +15,18 @@ async function pageLoader(page) {
   const html = await response.text();
   mainContent.innerHTML = html;
 
+  //push the string to the URL
   history.pushState({ page: page }, null, `#${page.slice(11)}`);
 }
 
 shopLink.addEventListener("click", (e) => {
   e.preventDefault();
+  //get the data-page since the html file will be set there
   const page = shopLink.getAttribute("data-page");
+  //pass page to the pageloader function
   pageLoader(page);
 });
+
+//create the initial state for the URL
+pageLoader(homePage);
+console.log(window.location.hash);
