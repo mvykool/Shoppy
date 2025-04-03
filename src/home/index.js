106811e8c19613373eb8bbeff6e6ca585a1a9403
Maicol Hernandez <maykolandres4@gmail.com>
2025-04-03 12:00:00 -0400

    added variables and fetch function

--- src/home/index.js
+++ src/home/index.js
@@ -1,2 +1,13 @@
 //first get the container by its class
 const productsContainer = document.querySelector(".product-section");
+
+//fetch products, and store them in a variable
+let products = [];
+
+const fetchData = async () => {
+  try {
+    const response = await fetch("");
+
+    return response.json();
+  } catch (error) {}
+};
